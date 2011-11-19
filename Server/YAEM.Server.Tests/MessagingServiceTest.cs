e179532387df6552581ef959aa815d8d4f102355
Florian Amstutz <florian@amstutz.nu>
2011-11-19 12:38:56 +0100

    Added MessagingService Test

---
+++ Server/YAEM.Server.Tests/MessagingServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using YAEM.Server;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using YAEM.Domain;
+
+namespace YAEM.Server.Tests
+{
+    [TestClass()]
+    public class MessagingServiceTest
+    {
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+        
+
+    }
+}
